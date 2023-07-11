0ea78d3ddc72749c2cd9308f2acd199d3fffe0e2
Nathan Trautenberg <Natester123@mac.com>
2023-07-11 15:48:30 -0700

    Initial commit

---
+++ plants.rb
@@ -0,0 +1,9 @@
+# odds to thrive
+gets_sunshine = true
+ounces_of_water = 9
+# does the plant have sunshine AND get at least 10oz of water?
+if gets_sunshine == true && ounces_of_water >= 10
+puts "This plant will thrive."
+else
+    puts "This plant is toast!"
+end
