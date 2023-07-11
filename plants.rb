a98f82db0c80a7a6afc864334323a0e2057aaeec
Nathan Trautenberg <Natester123@mac.com>
2023-07-11 15:57:18 -0700

    add conditional

--- plants.rb
+++ plants.rb
@@ -7,3 +7,11 @@ puts "This plant will thrive."
 else
     puts "This plant is toast!"
 end
+
+# does the plant have sunshine OR get at least 10oz of water?
+if gets_sunshine == true && ounces_of_water >= 10
+puts "This plant will thrive."
+elsif gets_sunshine != true || ounces_of_water < 10
+        puts "This plant needs sunshine or more water!"
+else puts "This plant is toast!"
+end
